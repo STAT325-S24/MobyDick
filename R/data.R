19bc9d408af629b3e2d745adf578a3f30661837d
arogers24 <arogers24@amherst.edu>
2024-02-15 17:24:03 -0500

    Update with new variables

--- R/data.R
+++ R/data.R
@@ -7,7 +7,11 @@
 #' A data frame with 22243 rows and 1 column:
 #' \describe{
 #'   \item{text}{lines of text}
+#'   \item{line_number}{line number}
+#'   \item{section}{section of work the line is found}
+#'   \item{chapter_number}{chapter number}
 #'   ...
 #' }
 #' @source <https://www.who.int/teams/global-tuberculosis-programme/data>
 "MobyDick"
+
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -7,7 +7,17 @@ my_mirror <- "http://mirror.csclub.uwaterloo.ca/gutenberg/"
 mobydick <- gutenberg_download(15, mirror = my_mirror)
 
 MobyDick <- tibble(mobydick) |>
-  select(text)
+  select(text) |>
+  mutate(line_number = row_number(),
+         section = ifelse(row_number() < 10, "title", 
+                          ifelse(row_number() < 155, "table_of_contents",
+                                 ifelse(row_number() < 197, "etymology",
+                                        ifelse(row_number() < 664, "extracts",
+                                               "text")))),
+         chapter_number = cumsum(str_detect(text, 
+                                     regex("^CHAPTER [\\dIVXLC]",
+                                           ignore_case = TRUE))))
+
 
 usethis::use_data(MobyDick, overwrite = TRUE)
 
