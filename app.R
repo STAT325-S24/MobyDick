bf767225ec8d4c10b60b0ceb0ad7a7a2d1704bca
arogers24 <arogers24@amherst.edu>
2024-03-08 11:34:12 -0500

    Add scatter plot and clean table

--- app.R
+++ app.R
@@ -1,6 +1,9 @@
 library(shiny)
 library(readr)
 library(dplyr)
+library(plotly)
+library(stringr)
+library(DT)
 
 # Load in necessary data
 token_type_summary <- read_csv("token_type_summary.csv")
@@ -28,7 +31,8 @@ ui <- fluidPage(
     mainPanel(
       
       # display table
-      dataTableOutput(outputId = "table")
+      dataTableOutput(outputId = "table"),
+      plotlyOutput(outputId = "plot")
       
     )
   )
@@ -38,12 +42,24 @@ server <- function(input, output) {
   
   # table output
   output$table <- renderDataTable({
-    token_type_summary |>
+    to_display <- token_type_summary |>
       select(chapter_number,
              total,
              paste0(input$part_of_speech, "_count"),
              paste0(input$part_of_speech, "_prop")
-             )
+             ) |>
+      arrange(desc(get(paste0(input$part_of_speech, "_prop"))))
+    datatable(to_display, options = list(pageLength = 5))
+      
+  })
+  output$plot <- renderPlotly({
+    fig <- plot_ly(data = token_type_summary, 
+                   x = ~chapter_number, 
+                   y = ~get(paste0(input$part_of_speech, "_prop")), 
+                   type = "scatter") %>%
+      layout(title = 'Proportion of Parts of Speech by Chapter', xaxis = list(title = "Chapter Number"), 
+             yaxis = list(title = paste0('Proportion of ', str_to_title(input$part_of_speech))))
+    
   })
 }
 
