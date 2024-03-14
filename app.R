bec3f9af2ed386f0db0e99c32cd892700b3243d0
arogers24 <arogers24@amherst.edu>
2024-03-14 11:51:09 -0400

    Updates to add annotated text

--- app.R
+++ app.R
@@ -8,6 +8,7 @@ library(MobyDick)
 
 # Load in necessary data
 token_type_summary <- read_csv("token_type_summary.csv")
+token_with_chapters <- read_csv("token_with_chapters.csv")
 
 ui <- fluidPage(
   
@@ -20,18 +21,17 @@ ui <- fluidPage(
       # Buttons to select part of speech
       radioButtons(inputId = "part_of_speech",
                    h3("Select a Part of Speech"),
-                   choices = list("Noun" = "noun", 
-                                  "Adjective" = "adj",
-                                  "Verb" = "verb",
-                                  "Proper Noun" = "proper_noun",
-                                  "Conjunction" = "conj"), 
-                   selected = "noun")
+                   choices = list("Noun" = "NOUN", 
+                                  "Adjective" = "ADJ",
+                                  "Verb" = "VERB",
+                                  "Proper Noun" = "PROPN",
+                                  "Conjunction" = "CONJ"), 
+                   selected = "NOUN")
       
     ),
     
     mainPanel(
       
-      # display table
       dataTableOutput(outputId = "table"),
       plotlyOutput(outputId = "plot"),
       tags$style(HTML(".output_pre { max-height: 300px; }")),
@@ -55,24 +55,49 @@ server <- function(input, output) {
     datatable(to_display, options = list(pageLength = 5))
       
   })
+  
+  # plot output
   output$plot <- renderPlotly({
     fig <- plot_ly(data = token_type_summary, 
                    x = ~chapter_number, 
                    y = ~get(paste0(input$part_of_speech, "_prop")), 
                    type = "scatter",
+                   mode='markers',
                    source = "myPlotSource") |>
-      layout(title = 'Proportion of Parts of Speech by Chapter', xaxis = list(title = "Chapter Number"), 
-             yaxis = list(title = paste0('Proportion of ', str_to_title(input$part_of_speech))))
+      layout(title = 'Proportion of Parts of Speech by Chapter', 
+             xaxis = list(title = "Chapter Number"), 
+             yaxis = list(title = paste0('Proportion of ', 
+                                         str_to_title(input$part_of_speech))))
     event_register(fig, "plotly_click")
     fig
   })
   
+  # text output
   output$selected_point <- renderPrint({
     selected_point <- event_data("plotly_click", source = "myPlotSource")
     if (!is.null(selected_point)) {
-      to_print <- MobyDick::MobyDick |>
-        filter(chapter_number == selected_point[2]$pointNumber + 1)
-      HTML(paste(to_print$text, collapse = "<br>"))
+      token_with_chapters2 <- token_with_chapters |>
+        filter(chapter_number == selected_point[2]$pointNumber + 1) |>
+        mutate(token = 
+                 if_else(upos %in% ifelse(input$part_of_speech == "CONJ", 
+                                          c("CCONJ", "SCONJ"), 
+                                          c(input$part_of_speech)), 
+                         paste0("<b>", token, "</b>"), token))
+      
+      annotated_text <- token_with_chapters2 |>
+        arrange(doc_id, sid, tid) |> 
+        group_by(doc_id, sid) |>
+        mutate(space_after = ifelse(lead(upos, default = " ") == "PUNCT", 
+                                    "", " "),
+               word_with_space = paste(token, space_after, sep = "")) |>
+        summarise(sentence = paste(word_with_space, collapse = ""), 
+                  .groups = 'drop') |> 
+        group_by(doc_id) |>
+        summarise(paragraph = paste(sentence, collapse = " "), 
+                  .groups = 'drop')
+      
+      # Output the result
+      HTML(paste(annotated_text$paragraph, collapse = ""))
     } else {
       "Click on a point to see its information"
     }
